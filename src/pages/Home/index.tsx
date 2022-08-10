7f68994240b0d60d0d498e6acc09622d5d13ac0a
Vinicius de Oliveira Pinto <viniciusoliveira9699@gmail.com>
2022-08-10 19:17:35 -0300

    add api connection to consume post list data

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,74 +3,55 @@ import { Post } from '../../components/Post';
 import { ProfileSettings } from '../../components/ProfileSettings';
 
 import styles from './styles.module.css';
+import { useEffect, useState } from 'react';
 
-// interface HomeProps {
-//   user: string;
-//   photo: string;
-// }
-
-const posts = [
-  {
-    id: 0,
-    author:{
-      avatarUrl: 'https://github.com/vinioliver01.png',
-      name: 'Vinicius',
-      role: 'Web Developer',
-    },
-    content:[
-      {type: 'paragraph', content: 'Fala galeraa 👋'},
-      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. e um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
-      {type: 'link', content: '👉 jane.design/doctorcare'},
-      {type: 'link', content: '#novoprojeto #nlw #rocketseat'},
-    ],
-    publishedAt: new Date('2022-07-11 20:53:00'),
-    commentId: [0,1]
-  },
-  {
-    id: 1,
-    author:{
-      avatarUrl: 'https://github.com/diego3g.png',
-      name: 'Diego',
-      role: 'Rocket CTO',
-    },
-    content:[
-      {type: 'paragraph', content: 'Fala galeraa 👋'},
-      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
-      {type: 'link', content: '👉 jane.design/doctorcare'},
-      {type: 'link', content: '#novoprojeto #nlw #rocketseat'},
-    ],
-    publishedAt: new Date('2022-07-13 18:53:00'),
-    commentId: [0]
-  }
-];
+interface HomeProps {
+  user: string;
+  photo: string;
+}
 
 const loggedUser = {
     userName: 'Vinicius',
     userPhoto:'https://github.com/vinioliver01.png',
   }
 
+const url = "http://localhost:3333"
 
 export function Home(){
+  const [listPost, setListPost] = useState([])
+
+  useEffect(() => {
+    fetch(`${url}/posts`)
+    .then(response => response.json())
+    .then(data => {
+      if(listPost.length==0) {
+        setListPost(data.map(item => item))
+      }
+    })
+    
+  }, [])
 
   return (
     <div className={styles.container}>
       <NavBar />
       <div className={styles.main}>
         <div className={styles.asside}>
-          <ProfileSettings user={loggedUser.userName} photo={loggedUser.userPhoto} />
+          <ProfileSettings user={loggedUser.userName} photo={loggedUser.userPhoto}/>
         </div>
         <div className={styles.body}>
-          {posts.map(post =>{
+          
+          {listPost.map(post =>{
             return (
               <Post 
               key={post.id}
               author={post.author}
               content={post.content}
-              publishedAt={post.publishedAt}
+              publishedAt={new Date(post.publishedAt)}
               commentId={post.commentId}
               userName={loggedUser.userName}
               userPhoto={loggedUser.userPhoto}
               />
+              
             )
           })}
         </div>
